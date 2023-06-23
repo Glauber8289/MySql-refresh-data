98cc5199ec659b83183157d6812a9486615a82a9
Glauber8289 <Petersonglauber@hotmmail.com>
2023-06-22 22:28:17 -0300

    Atualizando dados

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -15,47 +15,25 @@ import db.DB;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-		Connection conn= null;
+		Connection conn = null;
 		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
+			st= conn.prepareStatement(
+                  "UPDATE seller "
+				  + "SET BaseSalary = BaseSalary +  ?" 
+                  + "WHERE "
+				  + "(DepartmentId = ?)");
 			
-			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name,Email,BirthDate,BaseSalary,DepartmentId) "
-					+ "VALUES "		
-					+ "(?,?,?,?,?)",
-					Statement.RETURN_GENERATED_KEYS);
+			st.setDouble(1,200);
+			st.setInt(2, 2);
 			
-			st.setString(1, "Joao dos Santos");
-			st.setString(2, "JoaodosSantos@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("17/03/1992").getTime()));
-			st.setDouble(4, 3000);
-			st.setInt(5, 4);
-			
-		int rowsAffected=st.executeUpdate();
-		if (rowsAffected > 0) {
-			ResultSet rs= st.getGeneratedKeys();
-			while (rs.next()) {
-				int id = rs.getInt(1);
-				System.out.println("Feito! ID = " + id);
-				
-			}
-		}else {
-			System.out.println("Nenhuma linha foi alterada!");
-		}
-		System.out.println("Feito! Linhas afetadas " + rowsAffected);
-			
-			
+			int rowsAffectted = st.executeUpdate();
+			System.out.println("Feito! Linhas afetadas: " + rowsAffectted);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
